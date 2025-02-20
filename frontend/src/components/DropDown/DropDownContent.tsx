100d29a50717edf873e145695076e53ef5ce25e9
dyandev <diego6mm@gmail.com>
2025-02-20 16:36:34 -0400

    Adding animation to DropDown

--- frontend/src/components/DropDown/DropDownContent.tsx
+++ frontend/src/components/DropDown/DropDownContent.tsx
@@ -14,18 +14,20 @@ export const DropdownContent = ({
   return (
     <div
       className={cn(
+        'transition-all h-0 duration-300 [transition-timing-function:cubic-bezier(0.69,-0.04,0.17,1.67)] [interpolate-size:_allow-keywords]',
         'border',
-        open ? '' : 'opacity-0 pointer-events-none',
+        open ? 'h-auto duration-300 transition-all' : 'pointer-events-none',
+        open ? 'opacity-100' : 'opacity-0',
         open ? 'shadow-2xl shadow-black/40' : '',
         'rounded-sm',
-        'border-gray',
         'bg-[var(--bg-color)]',
+        'border-gray',
         'absolute left-0 mt-4 min-w-full flex flex-col items-center max-h-[45vh] overflow-y-scroll [-ms-overflow-style:_none] ',
         '[&::-webkit-scrollbar]:w-0 [&::-webkit-scrollbar]:h-0',
         className
       )}
       {...props}>
-      <>{open && children}</>
+      <>{children}</>
     </div>
   )
 }
