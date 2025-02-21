75d962e40c251422c232a55cbc7cd1e88c804164
dyandev <diego6mm@gmail.com>
2025-02-21 03:17:19 -0400

    Adding enbolivares-api url

--- frontend/src/services/pydolar.ts
+++ frontend/src/services/pydolar.ts
@@ -1,5 +1,7 @@
 import { Currency, MonitorsData } from '../types'
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.PROD
+  ? import.meta.env.VITE_ENBOLIVARES_API_URL
+  : import.meta.env.VITE_API_URL
 
 export class PyDolar {
   static PYDOLAR_URL = API_URL
